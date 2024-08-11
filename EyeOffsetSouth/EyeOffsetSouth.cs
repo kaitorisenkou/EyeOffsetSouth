b92155448b23eb5ddeee4428a255547e954ef014
kaitorisenkou <kaitorisenkou@icloud.com>
2024-08-11 19:31:42 +0900

    プロジェクト ファイルを追加します。

---
+++ EyeOffsetSouth/EyeOffsetSouth.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace EyeOffsetSouth {
+    [StaticConstructorOnStartup]
+    public class EyeOffsetSouth {
+        static EyeOffsetSouth() {
+            Log.Message("[EyeOffsetSouth] Now active");
+            var harmony = new Harmony("kaitorisenkou.EyeOffsetSouth");
+            harmony.Patch(
+                AccessTools.Method(typeof(PawnDrawUtility), nameof(PawnDrawUtility.CalcAnchorData), null, null),
+                null,
+                new HarmonyMethod(typeof(EyeOffsetSouth), nameof(Patch_CalcAnchorData), null),
+                null,
+                null
+                );
+            Log.Message("[EyeOffsetSouth] Harmony patch complete!");
+        }
+
+        public static void Patch_CalcAnchorData(Pawn pawn, BodyTypeDef.WoundAnchor anchor, Rot4 pawnRot, ref Vector3 anchorOffset) {
+            if ((anchor.tag == "LeftEye" || anchor.tag == "RightEye") && pawnRot.AsInt==2 && (!ModsConfig.BiotechActive || !pawn.DevelopmentalStage.Juvenile())) {
+                Log.Message("[EyeOffsetSouth] " + anchorOffset);
+                var modExt = pawn.story.headType.GetModExtension<ModExtension_EyeOffsetSouth>();
+                if (modExt != null) {
+                    anchorOffset += modExt.offset;
+                }
+            }
+        }
+    }
+}
---
+++ EyeOffsetSouth/ModExtension_EyeOffsetSouth.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace EyeOffsetSouth {
+    public class ModExtension_EyeOffsetSouth : DefModExtension {
+        public Vector3 offset = Vector3.zero;
+    }
+}
